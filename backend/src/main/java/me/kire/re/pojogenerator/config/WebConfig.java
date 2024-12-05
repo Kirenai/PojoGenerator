c820e7fdc10c110a30cda45840609ecedc99dd12
Kirenai <pool_dota2@hotmail.com>
2024-12-05 18:28:31 -0500

    feat(config): add env

--- backend/src/main/java/me/kire/re/pojogenerator/config/WebConfig.java
+++ backend/src/main/java/me/kire/re/pojogenerator/config/WebConfig.java
@@ -1,21 +1,31 @@
 package me.kire.re.pojogenerator.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebConfig {
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+    @Value("${allowed.methods}")
+    private String allowedMethods;
+    @Value("${allowed.headers}")
+    private String allowedHeaders;
+
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.addAllowedOrigin(this.allowedOrigin);
+        config.addAllowedHeader(this.allowedHeaders);
+        config.addExposedHeader(HttpHeaders.CONTENT_DISPOSITION);
+        config.addAllowedMethod(this.allowedMethods);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
