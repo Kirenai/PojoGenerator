c722b8302d191a7610b9f7159eb5b8607f2aa1ac
Kirenai <pool_dota2@hotmail.com>
2024-12-05 19:09:27 -0500

    refactor(env): rename env

--- frontend/src/modules/pojo/infrastructure/config/axiosConfig.ts
+++ frontend/src/modules/pojo/infrastructure/config/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:8080/api'
+const baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080/api'
 
 const instance = axios.create({
   baseURL: baseURL,
