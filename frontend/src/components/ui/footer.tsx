3c65cd81980de12a83e8d82a0e967b3b24299ae1
Kirenai <pool_dota2@hotmail.com>
2024-12-08 17:49:37 -0500

    refactor(footer): dynamically current year

--- frontend/src/components/ui/footer.tsx
+++ frontend/src/components/ui/footer.tsx
@@ -1,12 +1,14 @@
 import { Box, Container, HStack, Text, VStack } from '@chakra-ui/react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <Container py="8" flex="1" alignContent="end">
       <Box as="footer" fontSize={{ base: 'sm' }}>
         <HStack>
           <VStack alignItems="flex-start" gap="2">
-            <Text color="gray.400">Proyecto realizado por Kirenai © 2024</Text>
+            <Text color="gray.400">Proyecto realizado por Kirenai © {currentYear}</Text>
             <Text color="gray.400">Mantenido por Kirenai</Text>
           </VStack>
         </HStack>
