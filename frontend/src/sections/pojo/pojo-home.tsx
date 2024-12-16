0edf275f948f06cb6a553655556a03e0c89927ca
Kirenai <pool_dota2@hotmail.com>
2024-12-16 00:08:54 -0500

    feat(pojo): add setup menu

--- frontend/src/sections/pojo/pojo-home.tsx
+++ frontend/src/sections/pojo/pojo-home.tsx
@@ -1,10 +1,27 @@
-import { Box, Code, Container, HStack, Text, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
+import { GoGear } from 'react-icons/go'
+import { IoTelescopeOutline } from 'react-icons/io5'
+import { Box, Code, Container, HStack, Icon, Text } from '@chakra-ui/react'
+import { Button } from '@/components/ui/button'
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from '@/components/ui/menu'
 import PojoForm from './components/PojoForm'
 
 const PojoHome = () => {
+  const [setup, setSetup] = useState({ lombok: false })
+
+  const handleOnClick = () => {
+    console.log('Click')
+    setSetup({ lombok: !setup.lombok })
+  }
+
   return (
     <Container p="4">
-      <VStack gap="0.5" py="2">
+      <HStack gap="0.5" py="2" justifyContent="space-evenly">
         <Box w="auto" fontSize={{ lg: 'lg' }}>
           <HStack justifyContent="space-between">
             <Text>Objeto de entrada: </Text>
@@ -15,7 +32,33 @@ const PojoHome = () => {
             <Code fontSize="md">data[n].person.id - O</Code>
           </HStack>
         </Box>
-      </VStack>
+        <Box>
+          <MenuRoot>
+            <MenuTrigger asChild>
+              <Button disabled variant="surface" colorPalette="teal">
+                <GoGear />
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem
+                value="lombok"
+                valueText="lombok"
+                onClick={handleOnClick}
+              >
+                <Icon fontSize="lg">
+                  <IoTelescopeOutline />
+                </Icon>
+                <Text
+                  color={setup.lombok ? 'teal.300' : 'gray.100'}
+                  fontSize="lg"
+                >
+                  Lombok
+                </Text>
+              </MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        </Box>
+      </HStack>
       <PojoForm />
     </Container>
   )
