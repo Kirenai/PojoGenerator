fe0850bc90c7b6b536c8f5675dce638b459039dc
Kirenai <pool_dota2@hotmail.com>
2025-01-08 18:49:14 -0500

    feat(config): remove unused imports

--- backend/src/main/java/me/kire/re/pojogenerator/config/WebConfig.java
+++ backend/src/main/java/me/kire/re/pojogenerator/config/WebConfig.java
@@ -1,15 +1,12 @@
 package me.kire.re.pojogenerator.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
